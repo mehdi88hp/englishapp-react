15b080bdafea24be4c274aacc80626038ddb790d
MehdiHP <mmhp16@gmail.com>
2022-08-09 11:52:35 +0430

    implement redux and redux-toolkit with a dummy counter example

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import React from 'react'
 import type { NextPage } from 'next'
+import { store } from '../../store'
+import { Provider } from 'react-redux'
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: React.ReactElement) => React.ReactNode
@@ -25,7 +27,11 @@ export default function MyApp({Component, pageProps}: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 // export default MyApp
--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -6,13 +6,24 @@ import type { ReactElement } from 'react'
 import Layout from './layout'
 import type { NextPageWithLayout } from '../_app'
 import Link from "next/link";
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from "@slices/counterSlice";
+import { selectCount } from "@slices/counterSlice";
+
 
 const Page: NextPageWithLayout = () => {
+
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
+        <button className="btn btn-active" onClick={() => dispatch(increment())}>increment</button>
+        <button className="btn btn-active" onClick={() => dispatch(decrement())}>decrement</button>
+
         <Link href={`/admin/books`}>
-          <a>testtttt</a>
+          <a>testtttt {count}</a>
         </Link>
         <Paper sx={{p: 2, display: 'flex', flexDirection: 'column'}}>
           <Orders/>
